971cc7ceb2896389861fbd546d2b33816992378a
Naira <naira.masgo@gmail.com>
2021-08-13 22:04:41 -0500

    Version 2.0

--- src/test/java/page/TestEdu.java
+++ src/test/java/page/TestEdu.java
@@ -1,6 +1,9 @@
 package page;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestEdu {
@@ -17,5 +20,20 @@ public class TestEdu {
         driver.get("https://www.edureka.co/");
         String tituloWeb = driver.getTitle();
         System.out.println(tituloWeb + "\n");
+    }
+
+    public void buscarCurso(){
+        WebElement txtEnterBuscar=driver.findElement(By.className("open_search_overlay"));
+        WebElement txtEnterXpath=driver.findElement(By.xpath("//div[@class='search_header']/input"));
+        txtEnterXpath.click();
+        WebElement txtCurso= driver.findElement(By.id("search-input"));
+        txtCurso.clear();
+        txtCurso.sendKeys("Java");
+        txtCurso.sendKeys(Keys.ENTER);
+    }
+    public static void main(String[] args) {
+        TestEdu testEdu= new TestEdu();
+        testEdu.invocarNavegador();
+        testEdu.buscarCurso();
     }
 }
